c3239249abaf5b8dea634091e960015bbc1e15c5
bbarber <bbarber@coastalcomputerservices.net>
2015-10-14 09:48:16 -0400

    updated app.js to include game winner code

--- tictac/app.js
+++ tictac/app.js
@@ -2,107 +2,73 @@
 
 // alert('What is your name');
 
-var letter = 'X';
+// choose the first player to move
+//
 
+var currentPlayer = "X";
+
+if (Math.random() > .50) {
+  currentPlayer = "O";
+};
+var numOfMoves = 0;
+var playerXMoveTotal = 0;
+var playerYMoveTotal = 0;
+
+var moveValues = [1, 2, 5, 13, 34, 89, 233, 610, 1597];
+
+var winningCombos = [8, 136, 247, 272, 646, 1632, 1691, 2440];
+
+//
+// Process a player move
+//
 $(document).ready(function() {
+
   $('.square').on('click', function(){
-    var firstSquare = $(this).text(letter);
-    alert('sqaure one clicked');
+    var moveValue = parseInt($(this).text());
+
+    $(this).text(currentPlayer);
+
+    $(this).off();
+    numOfMoves ++;
+    // ajax callout here to send move to server
+
+    if (currentPlayer === "O") {
+      playerYMoveTotal = playerYMoveTotal + moveValue;
+      currentPlayer = "X";
+    } else {
+      playerXMoveTotal = playerXMoveTotal + moveValue;
+      currentPlayer = "O";
+    };
+
+   if (numOfMoves >= 5) {checkWinner(playerXMoveTotal, playerYMoveTotal, numOfMoves)};
   });
 });
 
+var checkWinner = function checkWinner(num1, num2, moves) {
+  var winner = null;
+
+  for (var i = 0; i < winningCombos.length; i++) {
+    if (num1 === winningCombos[i]) {
+      winner = 'playerX';
+      alert('Player X is has won');
+    } else if (num2 === winningCombos[i]) {
+        winner = 'playerY';
+        alert('Player Y has won');
+      };
+      // else if (moves === 9) {
+      //   winner = 'tie';
+      //   alert('This game has ended in a tie');
+      // };
+  };
+  if ((winner === null) && (moves === 9)) {
+    alert('this game is a tie');
+  };
+  return winner;
+};
+
+ alert(playerXMoveTotal, playerYMoveTotal);
 
 
-alert(firstSquare);
-
-// var init = function init() {
-//   var yourName = prompt('What is your name?')
-// };
-
-//  document.getElementById('name').textContent = yourName;
-
-// window.addEventListener('load', init);
-
-var trackingArray = [['', '', ''],
-                      ['', '', ''],
-                      ['', '', '']];
-
-
-
-var X = 'X';
-var Y = 'Y';
-
-var click = $('.one').on('click', function(){
-    $('.one').text('X');
-});
-
-
-
-// var async = require('async');
-
-// var readline = require('readline');
-// var io = readline.createInterface({
-//   input: process.stdin,
-//   output: process.stdout
-// });
-
-// io.setPrompt('> ');
-
-// var TTT = require('./lib/tic-tac-toe.js');
-// var game = new TTT();
-
-// var drawBoard = function(game) {
-//   var cells = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-//   cells.forEach(function(cell, i, array) {
-//     array[i] = ' ' + (game.whoPlayedAt(cell) || cell) + ' ';
-//   });
-
-//   return [
-//     cells.slice(0, 3).join('|'),
-//     '---+---+---',
-//     cells.slice(3, 6).join('|'),
-//     '---+---+---',
-//     cells.slice(6, 9).join('|')
-//   ].join('\n');
-// };
-
-// process.stdout.write('Welcome to Tic-Tac-Toe\n\n');
-
-// var moveCount = 0;
-
-// var gameShouldContinue = function() {
-//   return !game.isOver();
-// };
-
-// var playTurn = function(done) {
-//   var player = game.whoseTurn();
-
-//   process.stdout.write(drawBoard(game));
-//   process.stdout.write('\n\nIt is ' + player + '\'s turn\n\n');
-
-//   io.question('Where do you wish to play?', function(answer) {
-//     var num = parseInt(answer, 10);
-
-//     if (game.isMoveLegal(player, num)) {
-//       game.move(player, num);
-//     } else {
-//       process.stdout.write('That is not a legal move.\n');
-//     }
-
-//     if (game.hasWon(player)) {
-//       process.stdout.write('Player ' + player + ' wins!\n\n');
-//     } else if (game.boardIsFull()) {
-//       process.stdout.write('The game is a tie.\n\n');
-//     }
-
-//     done();
-//   });
-// };
-
-// var gameDone = function(done) {
-//   process.stdout.write('Thank you for playing.\n');
-//   io.close();
-// };
 
 // async.whilst(
 //   gameShouldContinue,
